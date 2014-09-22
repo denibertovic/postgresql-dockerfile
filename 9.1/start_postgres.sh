737c0456e65bced83bf689451d29689d79dcf3fd
Deni Bertovic <deni@kset.org>
2014-09-22 13:34:12 +0200

    fix 9.1 and 9.2 to start properly as user postgres

--- 9.1/start_postgres.sh
+++ 9.1/start_postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /bin/su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf"
+exec /usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf
--- 9.2/start_postgres.sh
+++ 9.2/start_postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /bin/su postgres -c "/usr/lib/postgresql/9.2/bin/postgres -D /var/lib/postgresql/9.2/main -c config_file=/etc/postgresql/9.2/main/postgresql.conf"
+exec /usr/lib/postgresql/9.2/bin/postgres -D /var/lib/postgresql/9.2/main -c config_file=/etc/postgresql/9.2/main/postgresql.conf
