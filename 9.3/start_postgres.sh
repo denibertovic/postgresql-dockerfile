b7b40fa6c4cb3a383f3c8627846dd841fa27c3c2
Deni Bertovic <deni@kset.org>
2014-09-22 13:42:49 +0200

    downgrade 9.3 to wheezy and use pg repo
    also fix proper user and permissions while starting the service

--- 9.3/start_postgres.sh
+++ 9.3/start_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec /bin/su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
+exec /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
 
